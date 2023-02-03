1bde4dc171affe130ebffc4052ed0d0ff546856e
Proximacentaurion <cristiancvn63@gmail.com>
2023-02-03 15:52:38 -0500

    Commit 5 Cristian Alvarado

--- SistemaDeTransporteCAlvarado/SistemaDeTransporteCAlvarado/src/main/java/com/CristianAlvarado/SistemaDeTransporteCAlvarado/SistemaDeTransporte.java
+++ SistemaDeTransporteCAlvarado/SistemaDeTransporteCAlvarado/src/main/java/com/CristianAlvarado/SistemaDeTransporteCAlvarado/SistemaDeTransporte.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-
+@Service
 public class SistemaDeTransporte {
     private ArrayList<Conexion> conexiones;
     private ArrayList<Bus> buses;
@@ -24,28 +24,52 @@ public class SistemaDeTransporte {
     public void agregarConexion(int numero, String origen, String destino){
         Conexion nuevaConexion=new Conexion(numero,origen,destino);
         conexiones.add(nuevaConexion);
+        System.out.println("La conexión "+origen+"->"+destino+" ha sido creada.");
     }
     public void agregarBus(int numero, int capacidad, String tipo){
         Bus nuevoBus=new Bus(numero,capacidad,tipo);
         buses.add(nuevoBus);
+        System.out.println("El bus tipo"+tipo+" con capacidad de "+capacidad+" pasajeros ha sido ingresado.");
     }
     public void agregarRuta(int numero, Conexion conexion, int tiempoRecorrido, int horaSalida, int horaLlegada){
         Ruta nuevaRuta=new Ruta(numero,conexion,tiempoRecorrido,horaSalida,horaLlegada);
         rutas.add(nuevaRuta);
+        System.out.println("La ruta número "+numero+" de "+conexion.getOrigen()+" ha "+conexion.getDestino()+" con hora de salida "+horaSalida+":00 GMT ha sido creada");
     }
     public void agregarPasajero(String nombre, int cedula){
         Pasajero nuevoPasajero=new Pasajero(nombre,cedula);
+        pasajeros.add(nuevoPasajero);
+        System.out.println("El pasajero "+nombre+" ha sido creado.");
     }
     /**
      * Se crea un nuevo viaje y se agrega a la lista de viajes
      */
     public void agregarViaje(int numero, Pasajero pasajero, String estado, Ruta ruta,int valor,Bus bus, Silla silla) {
         Viaje nuevoViaje=new Viaje(numero,pasajero,estado,ruta,valor,bus,silla);
+        viajes.add(nuevoViaje);
+        System.out.println("El viaje número "+numero+" ha sido vendido por un valor de "+valor+" pesos\n"
+        +"hora de salida "+ruta.getHoraSalida()+":00 GMT");
     }
-
+    public Conexion getConexion(int numero){
+        Conexion conexion=conexiones.get(numero);
+        return conexion;
+    }
+    public Bus getBus(int numero){
+        Bus bus=buses.get(numero);
+        return bus;
+    }
+    public Ruta getRuta(int numero){
+        Ruta ruta=rutas.get(numero);
+        return ruta;
+    }
+    public Pasajero getPasajero(int numero){
+        Pasajero pasajero=pasajeros.get(numero);
+        return pasajero;
+    }
+    public ArrayList<Conexion> getConexiones() {
+        return conexiones;
+    }
     public void venderViaje(){
-
-
     }
     public void generarTicket(){
 
--- SistemaDeTransporteCAlvarado/SistemaDeTransporteCAlvarado/src/main/java/com/CristianAlvarado/SistemaDeTransporteCAlvarado/SistemaDeTransporteCAlvaradoApplication.java
+++ SistemaDeTransporteCAlvarado/SistemaDeTransporteCAlvarado/src/main/java/com/CristianAlvarado/SistemaDeTransporteCAlvarado/SistemaDeTransporteCAlvaradoApplication.java
@@ -1,5 +1,6 @@
 package com.CristianAlvarado.SistemaDeTransporteCAlvarado;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -21,11 +22,36 @@ public class SistemaDeTransporteCAlvaradoApplication {
 
 		SpringApplication.run(SistemaDeTransporteCAlvaradoApplication.class, args);
 
+		//@Autowired
+		//public SistemaDeTransporte sistemaDeTransporte;
 		SistemaDeTransporte sistemaDeTransporte = new SistemaDeTransporte();
 		System.out.println("Aplicación para el control y gestión de los viajes y horarios");
 
 		sistemaDeTransporte.agregarConexion(1, "Medellín", "Cúcuta");
 		sistemaDeTransporte.agregarConexion(2, "Medellín", "Bogotá");
 		sistemaDeTransporte.agregarConexion(3, "Medellín", "Cali");
+
+		sistemaDeTransporte.agregarBus(1335, 45, "diamante");
+		sistemaDeTransporte.agregarBus(0355, 36, "platino");
+
+		Bus bus1 = sistemaDeTransporte.getBus(0);
+		Bus bus2 = sistemaDeTransporte.getBus(1);
+
+		Conexion conexion1 = sistemaDeTransporte.getConexion(0);
+		Conexion conexion2 = sistemaDeTransporte.getConexion(1);
+		Conexion conexion3 = sistemaDeTransporte.getConexion(2);
+
+		sistemaDeTransporte.agregarRuta(1, conexion2, 12, 8, 20);
+		sistemaDeTransporte.agregarRuta(2, conexion2, 12, 10, 22);
+		sistemaDeTransporte.agregarRuta(3, conexion2, 12, 12, 24);
+
+		Ruta ruta1 = sistemaDeTransporte.getRuta(1);
+
+		sistemaDeTransporte.agregarPasajero("Juan Laverde", 129230923);
+		Pasajero pasajero1 = sistemaDeTransporte.getPasajero(0);
+
+		Silla silla1=new Silla(6,"vacía","Poltrona","pasillo");
+
+		sistemaDeTransporte.agregarViaje(1,pasajero1,"vendido",ruta1,35000,bus1,silla1);
 	}
 }
